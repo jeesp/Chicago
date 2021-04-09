2eb5c5ac18613b14013f6c7e96dfcac2ffd5ca8e
Jesper Grönroos <jesper.gronroos@hotmail.com>
2021-04-09 15:13:33 +0300

    Tikki luotu, pisteytys luotu, pelin jatkuvuus luotu

--- src/app.py
+++ src/app.py
@@ -12,11 +12,15 @@ def main():
     players.append(player3)
     player4 = Player('Åke')
     players.append(player4)
-    tulostaulukko = []
+    score_board = dict()
+    for player in players:
+        score_board[player] = 0
+    print(score_board)
     hand_values = {0:"Ei mitään", 1:"Pari", 2:"Kaksi paria", 3:"Kolmoset", 4:"Suora", 5:"Väri", 6:"Täyskäsi", 8:"Neloset", 10:"Värisuora"}
     points = 0
+    deals = 0
+    dealing_turn = 0
     while True:
-        turn = 0
         deck = Deck()
         random.shuffle(deck.cards)
         deck.deal_cards(players)
@@ -24,84 +28,131 @@ def main():
         for player in players:
             hand = player.hand_value()
             hands.append((player, hand))
-        deals = 0
-        #while deals < 1:
-        #    random.shuffle(deck.cards)
-        #    deck.deal_cards(players)
-        #    hands = []
-        #    for player in players:
-        #        hand = player.hand_value()
-        #        hands.append((player, hand))
-        #    comparing = compare_hands(hands)
-        #    if comparing[0] != 0:
-        #        if comparing[2] == 0:
-        #            print("Pisteet sai: " +comparing[0].name + ". Käsi: " + hand_values[comparing[1]].lower() + ".")
-        #        if comparing[2] == 1:
-        #            print("Pisteet sai: " +comparing[0].name + ". Käsi: " + hand_values[comparing[1]].lower() + ". Myös toisella pelaajalla oli " + hand_values[comparing[1]].lower() + ".")
-        #    else:
-        #        if comparing[2] == 2:
-        #            print("Kahdella pelaajalla on sama käsi: " + hand_values[comparing[1]].lower() + ". Kukaan ei saanut pisteitä.")
-        #        else:
-        #            print("Kenelläkään ei ollut mitään, joten kukaan ei saanut pisteitä.")
-        #    for hand in hands:
-        #        print (hand[0].name+"n käsi: ", hand_values[hand[1][0]].lower())
-        #    print("Valitse vaihdettavat kortit: ")
-        #    for hand in hands:
-        #        print(hand[0].name + ", mitä vaihdetaan? Anna indeksi")
-        #        i = 0
-        #        while i < len(hand[0].hand):
-        #            print(str(i) + ": " + hand[0].hand[i][2])
-        #            i += 1
-        #        changecards_ids = input("Syötä indeksit pilkulla erotettuna: ")
-        #        print (changecards_ids)
-        #        changedcards_ids_as_list = changecards_ids.split(",")
-        #    
-        #        print(changedcards_ids_as_list)
-        #        changecards = []
-        #        for id in changedcards_ids_as_list:
-        #            changecards.append(hand[0].hand[int(id)])
-        #        for card in changecards:
-        #            deck.add_card_to_dealt_cards(card)
-        #            hand[0].remove_card(card)
-        #        print (changecards)
-        #    deals += 1
-        played_card_index = input(players[turn].name + ", mikä kortti pelataan? Anna indeksi")
-        played_card = players[turn].hand[played_card_index]
-        players[turn].hand.remove(played_card)
-        
-        played_card_number = played_card[0]
-        played_card_suit = played_card[1]
-
-        compared_card_number = played_card_number
-        compared_card_suit = played_card_suit
-        compared_card_player = players[turn]
-
+        while deals < 2:
+            random.shuffle(deck.cards)
+            deck.deal_cards(players)
+            hands = []
+            for player in players:
+                hand = player.hand_value()
+                hands.append((player, hand))
+            value_comparison = compare_hands(hands)
+            if value_comparison[0] != 0:
+                score_board[value_comparison[0]] += value_comparison[1]
+                if value_comparison[2] == 0:
+                    print("Pisteet sai: " +value_comparison[0].name + ". Käsi: " + hand_values[value_comparison[1]].lower() + ".")
+                if value_comparison[2] == 1:
+                    print("Pisteet sai: " +value_comparison[0].name + ". Käsi: " + hand_values[value_comparison[1]].lower() + ". Myös toisella pelaajalla oli " + hand_values[value_comparison[1]].lower() + ".")
+            else:
+                if value_comparison[2] == 2:
+                    print("Kahdella pelaajalla on sama käsi: " + hand_values[value_comparison[1]].lower() + ". Kukaan ei saanut pisteitä.")
+                else:
+                    print("Kenelläkään ei ollut mitään, joten kukaan ei saanut pisteitä.")
+            print(score_board)
+            for hand in hands:
+                print (hand[0].name+"n käsi: ", hand_values[hand[1][0]].lower())
+            print("Valitse vaihdettavat kortit: ")
+            for hand in hands:
+                print(hand[0].name + ", mitä vaihdetaan? Anna indeksi")
+                i = 0
+                while i < len(hand[0].hand):
+                    print(str(i) + ": " + hand[0].hand[i][2])
+                    i += 1
+                changecards_ids = input("Syötä indeksit pilkulla erotettuna: ")
+                print (changecards_ids)
+                changedcards_ids_as_list = changecards_ids.split(",")
+                print(changedcards_ids_as_list)
+                cards_to_change = []
+                if changecards_ids != "":
+                    for id in changedcards_ids_as_list:
+                        cards_to_change.append(hand[0].hand[int(id)])
+                    for card in cards_to_change:
+                        deck.add_card_to_dealt_cards(card)
+                        hand[0].remove_card(card)
+                print (cards_to_change)
+            deals += 1
+        random.shuffle(deck.cards)
+        deck.deal_cards(players)
+        value_comparison = compare_hands(hands)
+        start = dealing_turn
         while len(players[0].hand) > 0:
-            start = turn
-            played_card_index = input(players[start].name + ", mikä kortti pelataan? Anna indeksi")
-            played_card = players[start].hand[played_card_index]
-
-            compared_card_number = played_card_number
-            compared_card_suit = played_card_suit
-            compared_card_player = players[start]
-            turn = start
+            compare_card = None
+            compare_card = play_card(players[start], compare_card)
+            print(compare_card)
+            turn = start + 1
+            if turn == len(players):
+                turn = 0
             while turn != start:
+                compare_card = play_card(players[turn], compare_card)
                 turn += 1
-                if turn == len(players)-1:
+                if turn == len(players):
                     turn = 0
-            played_card_index = input(players[turn].name + ", mikä kortti pelataan? Anna indeksi")
-            played_card = players[turn].hand[played_card_index]
+            start = players.index(compare_card[2])
+        dealing_turn += 1
+        deals = 0
+        if compare_card[0] == 2:
+            score_board[compare_card[2]] += 10
+            print("Kierroksen lopetti " + compare_card[2].name + " kakkoslopetuksella")
+        else:
+            score_board[compare_card[2]] += 5
+            print("Kierroksen lopetti " + compare_card[2].name)
+        
+        if dealing_turn == len(players):
+            dealing_turn = 0
+        print(score_board)
+        points = []
+        for player in players:
+            points.append(score_board[player])
+        points.sort()
+        if points[len(points)-1] >= 52:
+            if points[len(points)-1] > points[len(points)-2]:
+                for player in players:
+                    if score_board[player] == points[len(points)-1]:
+                        print("Peli päättynyt!")
+                        print("Voittaja on: " + player.name + ". Pisteet: " + str(points[len(points)-1]))
+            if points[len(points)-1] == points[len(points)-2]:
+                print("Pisteraja ylitetty, mutta osalla pelaajista on tasatilanne.")
+                print("Pelaajat tasatilanteessa: ", end=" ")
+                for player in players:
+                    if score_board[player] == points[len(points)-1]:
+                        print(player.name + " ", end=" ")
+                print("Pisteillä: " + str(points[len(points)-1]))
+            break
+        
+def set_compare_card(player,card):
+    compare_card_number = card[0]
+    compare_card_suit = card[1]
+    compare_card_player = player
+    return (compare_card_number, compare_card_suit, compare_card_player)
 
-            played_card_number = played_card[0]
-            played_card_suit = played_card[1]
+def play_card(player, compare_card):
+    i = 0
+    playable_cards = []
+    for card in player.hand:
+        if compare_card is not None:
+            if card[1] == compare_card[1]:
+                playable_cards.append(card)
+        else:
+            playable_cards.append(card)
+    if len(playable_cards) == 0:
+        playable_cards=player.hand
+    while i < len(player.hand):
+        if player.hand[i] in playable_cards:
+            print(str(i) + ": " + player.hand[i][2])
+        else:
+            print("X: " + player.hand[i][2])
+        i += 1
+    played_card_index = int(input(player.name + ", mikä kortti pelataan? Anna indeksi: "))
+    played_card = player.hand[played_card_index]
+    player.hand.remove(played_card)
+    print(played_card)
+    if compare_card == None:
+        return set_compare_card(player,played_card)
+    else:
+        if played_card[1] == compare_card[1]:
+            if played_card[0] > compare_card[0]:
+                return set_compare_card(player,played_card)
+    return compare_card
 
-            players[turn].hand.remove(played_card)
-
-            if played_card_suit == compared_card_suit:
-                if played_card_number > compared_card_number:
-                    compared_card_number = played_card_number
-
-        break
 def set_player(players, number):
     print(str(number)+ ". pelaajan nimi: ", end=" ")
     name = input()
