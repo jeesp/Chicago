823df9ca8811ef72cf20391eb9ed92f9723d0b77
Jesper Grönroos <jesper.gronroos@hotmail.com>
2021-05-15 17:11:24 +0300

    Käyttämätön task poistettu

--- tasks.py
+++ tasks.py
@@ -13,9 +13,6 @@ def build(ctx):
 def lint(ctx):
     ctx.run("pylint src")
 @task
-def format(ctx):
-    ctx.run("autopep8 --in-place --recursive src")
-@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src")
 @task(coverage)
